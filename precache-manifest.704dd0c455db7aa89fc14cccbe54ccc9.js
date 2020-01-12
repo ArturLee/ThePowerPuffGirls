9a8b8f90549f3a2467ad2ccaeab735ca266d88a6
Artur <arturlee_word@hotmail.com>
2020-01-13 00:32:41 +0100

    Updates

---
+++ precache-manifest.704dd0c455db7aa89fc14cccbe54ccc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bd9fb50f412dc50c9ec3f6ce588957",
+    "url": "/ThePowerPuffGirls/index.html"
+  },
+  {
+    "revision": "487142c77f78de7cc644",
+    "url": "/ThePowerPuffGirls/static/css/main.a7867567.chunk.css"
+  },
+  {
+    "revision": "b8c2a444d6db0f064279",
+    "url": "/ThePowerPuffGirls/static/js/2.aa6d3cb7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ThePowerPuffGirls/static/js/2.aa6d3cb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "487142c77f78de7cc644",
+    "url": "/ThePowerPuffGirls/static/js/main.da0905a3.chunk.js"
+  },
+  {
+    "revision": "69f1a391d5dcd89de1c6",
+    "url": "/ThePowerPuffGirls/static/js/runtime-main.74d6af30.js"
+  }
+]);
