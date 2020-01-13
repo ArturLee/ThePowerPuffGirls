1cc29ecfd4aff603f8765dca41bf3f1b2e74305c
Artur <arturlee_word@hotmail.com>
2020-01-13 10:35:14 +0100

    [*]fix github-Page

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const showUrl = 'http://api.tvmaze.com/shows/6771';
-export const episodeUrl = 'http://api.tvmaze.com/shows/6771/episodes';
+export const showUrl = 'https://api.tvmaze.com/shows/6771';
+export const episodeUrl = 'https://api.tvmaze.com/shows/6771/episodes';
